defdf58f8a18ac9441700e9d81e25f945542189d
Victor Jian Choe <vchoe89@gmail.com>
2019-05-20 17:20:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.size == 0
+    puts "The line is currently empty."
+  else
+    message = "The line is currently:"
+    array.each.with_index(1) do |name, i|
+      message << " #{i}. #{name}"
+    end
+    puts message
+  end
+end
+
+def take_a_number(array, name)
+  array.push(name)
+  position = array.index(name)
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+  return name, position 
+end
+
+def now_serving(array)
+  if array.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array.shift}."
+  end
+end
